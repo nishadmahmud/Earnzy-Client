cdc58117d6da91a8951d9566d5ac8b16eeb187cf
nishadmahmud <mahmudnishad253@gmail.com>
2025-07-14 00:29:11 +0600

    project setup

---
+++ src/routes/routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
